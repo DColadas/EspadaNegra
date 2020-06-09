2ab5242e49177ca2c89ea25a7117a484b1ad6a0b
DColadas <dcoladasmato@gmail.com>
2020-06-09 14:46:39 +0200

    Edit main to start server

--- src/main.cpp
+++ src/main.cpp
@@ -1,5 +1,18 @@
 #include <iostream>
 
-int main(int, char**) {
-    std::cout << "Hello, world!\n";
+#include "IO/Server.hpp"
+
+int main(int argc, char **argv) {
+    if (argc != 3) {
+        std::cerr << "Usage: " << argv[0] << " [ADDRESS] [PORT]\n"
+                  << "Example:\n\t" << argv[0] << " 127.0.0.1 8080\n";
+        return EXIT_FAILURE;
+    }
+    auto address = argv[1];
+    auto port = static_cast<unsigned short>(std::atoi(argv[2]));
+
+    Server s(address, port);
+    s.run();
+
+    return EXIT_SUCCESS;
 }
