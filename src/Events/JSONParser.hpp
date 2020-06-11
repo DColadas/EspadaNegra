8d4f1525752e50ef7c7e060995b690821d339745
DColadas <dcoladasmato@gmail.com>
2020-06-11 11:35:47 +0200

    Added some documentation to JSON functions

--- src/Events/JSONParser.hpp
+++ src/Events/JSONParser.hpp
@@ -10,10 +10,20 @@ namespace JSONParser {
 
 // Returns unique_ptr to GameEvent parsed from the message
 // Returns invalid GameEvent on error
+// Valid input messages:
+//  {"type": "joinMatchRequest", "matchID": "XXX", "nickname": "XXX"}
+//  {"type": "attack"}
+//  {"type": "pass"}
+//  {"type": "offer", "gold": int}
 std::unique_ptr<GameEvent> messageToGameEvent(Timestamp time, const std::string& nickname, const std::string& message);
 
 // Returns unique_ptr with the event in JSON
 // If the event is not valid, returns nullptr
+// Output messages:
+//  {"type": "pay", "nickname": "XXX", "gold": int}
+//  {"type": "earn", "nickname": "XXX", "gold": int}
+//  {"type": "winner", "nickname": "XXX"}
+//  {"type": "getCard", "nickname": "XXX"}
 std::unique_ptr<const std::string> gameEventToMessage(const GameEvent* event);
 
 }  // namespace JSONParser
