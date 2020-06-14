a0f269f7d291e119fcc5f8b091acf867e798f37c
DColadas <dcoladasmato@gmail.com>
2020-06-14 23:19:01 +0200

    Fixed bug linking Random

--- src/Utils/Random.cpp
+++ src/Utils/Random.cpp
@@ -1,23 +1,9 @@
 #include "Random.hpp"
 
 #include <random>
-#include <algorithm>
 
-namespace Random {
-namespace {
-std::random_device rd;  //Seed
-std::mt19937 rng{rd()};
-}  // namespace
-
-int randInt(int min, int max) {
+int Random::randInt(int min, int max) {
     //TODO panic if min < max
     std::uniform_int_distribution<> dist(min, max);
     return dist(rng);
 }
-
-template <class RandomIt>
-void shuffle(RandomIt first, RandomIt last) {
-    std::shuffle(first, last, rng);
-}
-
-}  // namespace Random
--- src/Utils/Random.hpp
+++ src/Utils/Random.hpp
@@ -1,15 +1,23 @@
 #pragma once
 
+#include <algorithm>
 #include <iterator>
+#include <random>
 
 // Utilities to generate random numbers
 namespace Random {
+namespace {
+std::random_device rd;  //Seed
+std::mt19937 rng{rd()};
+}  // namespace
 
 // Generate random integer between two given ones (included)
 int randInt(int min, int max);
 
 // Shuffle a collection of elements
 template <class RandomIt>
-void shuffle(RandomIt first, RandomIt last);
+void shuffle(RandomIt first, RandomIt last) {
+    std::shuffle(first, last, rng);
+}
 
 }  // namespace Random
