2a040bce5f45c2647db85767c9e3441a4de8974e
DColadas <dcoladasmato@gmail.com>
2020-06-21 12:08:45 +0200

    Logging in MatchManager

--- src/IO/MatchManager.cpp
+++ src/IO/MatchManager.cpp
@@ -1,10 +1,10 @@
 #include "MatchManager.hpp"
 
-#include <iostream>
 #include <thread>
 
 #include "Events/JoinMatchRequest.hpp"
 #include "Game/MatchHandler.hpp"
+#include "Logging/Logger.hpp"
 
 MatchManager::MatchManager() {
 }
@@ -37,17 +37,17 @@ MatchHandler* MatchManager::joinMatch(IOHandler& client, const JoinMatchRequest&
         //TODO get iterator while inserting
         matches[matchID] = std::make_unique<MatchHandler>();
         it = matches.find(matchID);
-        std::cout << "Match created - ";
+        LOG_INFO("Match created: ID = " + matchID);
     } else {
         //Match found, join
-        std::cout << "Match found - ";
     }
     if (!it->second->addPlayer(&client, nickname)) {
         // Couldn't join the match (full or existing username)
+        LOG_DEBUG("Couldn't join " + matchID + " by " + nickname + " (full or existing username)");
         return nullptr;
     }
 
-    std::cout << it->first << "::" << it->second->getPlayerCount() << std::endl;
+    LOG_DEBUG(nickname + " joins " + it->first + ", players " + std::to_string(it->second->getPlayerCount()));
     if (it->second->isFull()) {
         // If the match became full after joining, start it!
         //TODO starting threads for each match is far from ideal
